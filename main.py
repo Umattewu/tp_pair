0b315a9eb85890d0bc1826de60b851f8475ccb4b
Moja <marine.zouaoui@hotmail.com>
2020-12-13 20:30:38 +0100

    Refactor du code principal (renommage de certaines fonctions et variables) et refactor des tests (renommage des tests). TP terminé jusqu'à la feature 15.

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-def affiche_message(message):
-    tableau_de_nom_entrer = message.split(',')
+def afficher_message(message):
+    tableau_noms = message.split(',')
     if len(message.strip()) == 0:
         return "Hello, my friend"
-    elif(message.isupper()==1):
-        return (str("HELLO, ")+str(message)+str("!"))
-    elif len(tableau_de_nom_entrer) == 1:
-        return str("Hello, ") + str(tableau_de_nom_entrer[0].title())
+    elif message.isupper() == 1:
+        return str("HELLO, ") + str(message) + str("!")
+    elif len(tableau_noms) == 1:
+        return str("Hello, ") + str(tableau_noms[0].title())
     else:
         nom = ""
         nom_en_majuscule = ""
         cpt_maj = 0
         cpt_min = 0
-        tableau_entre_sans_doublon = []
         guests = []
-        for mot in tableau_de_nom_entrer:
-            mot = mot.strip()
-            if tableau_de_nom_entrer.count(mot) > 1:
-                mot = str(mot) + str("(x") + str(tableau_de_nom_entrer.count(mot)) + str(")")
-            if mot not in tableau_entre_sans_doublon:
-                tableau_entre_sans_doublon.append(mot)
+        tableau_entre_sans_doublons = sans_doublon(tableau_noms)
 
-        for mot in tableau_entre_sans_doublon:
+        for mot in tableau_entre_sans_doublons:
             mot = mot.strip()
             if mot[0] != '!':
                 if mot.isupper() == 1:
@@ -28,29 +22,36 @@ def affiche_message(message):
                     cpt_maj += 1
                 else:
                     cpt_min += 1
-                    if mot == tableau_entre_sans_doublon[-1] and len(nom) > 1:
+                    if mot == tableau_entre_sans_doublons[-1] and len(nom) > 1:
                         nom = str(nom) + str(" and ") + str(mot.title())
                     elif mot[0] == '*':
                         guests.append(mot[1:].title())
                     else:
                         nom = str(nom) + str(", ") + str(mot.title())
-        for guest in guests:
-            print(guest)
-        if len(guests) > 1:
-            nom = str(nom) + str(" and our specials guests ")
-            for guest in guests:
-                nom = str(nom) + str("&") + str(guest.title())
-        elif len(guests) == 1:
-            nom = str(nom) + str(" and our special guest ") + str(guests[0].title())
 
-        if "yoda" in tableau_entre_sans_doublon :
-            if cpt_min > 5:
-                return "World, Hello !"
-            elif cpt_maj > 5:
-                return "WORLD, HELLO !"
-            return str(nom[2:]) + str(", Hello")
-        if cpt_min > 5 :
+        if len(guests) == 1:
+            nom = str(nom) + str(" and our special guest ") + str(guests[0].title())
+        if "yoda" in tableau_entre_sans_doublons:
+            return message_yoda(cpt_min, cpt_maj, nom)
+        if cpt_min > 5:
             return "Hello, world !"
-        elif cpt_maj > 5 :
+        elif cpt_maj > 5:
             return "HELLO, WORLD !"
         return str("Hello") + str(nom) + str(nom_en_majuscule)
+
+def sans_doublon(tableau_noms):
+    tableau_sans_doublon = []
+    for mot in tableau_noms:
+        mot = mot.strip()
+        if tableau_noms.count(mot) > 1:
+            mot = str(mot) + str("(x") + str(tableau_noms.count(mot)) + str(")")
+        if mot not in tableau_sans_doublon:
+            tableau_sans_doublon.append(mot)
+    return tableau_sans_doublon
+
+def message_yoda(cpt_min, cpt_maj, nom):
+    if cpt_min > 5:
+        return "World, Hello !"
+    elif cpt_maj > 5:
+        return "WORLD, HELLO !"
+    return str(nom[2:]) + str(", Hello")
--- test_main.py
+++ test_main.py
@@ -1,28 +1,29 @@
-import main.main as m
+import main as m
 
-def test_affiche_message():
-    assert m.affiche_message("bob") == "Hello, Bob"
-    assert m.affiche_message("") == "Hello, my friend"
-    assert m.affiche_message(" ") == "Hello, my friend"
-    assert m.affiche_message("      ") == "Hello, my friend"
-    assert m.affiche_message("JERRY") == "HELLO, JERRY!"
+def test_affiche_message_features_1_2_and_2_with_spaces_and_3():
+    assert m.afficher_message("bob") == "Hello, Bob"
+    assert m.afficher_message("") == "Hello, my friend"
+    assert m.afficher_message(" ") == "Hello, my friend"
+    assert m.afficher_message("      ") == "Hello, my friend"
+    assert m.afficher_message("JERRY") == "HELLO, JERRY!"
 
-def test_affiche_plusieur_message():
-    assert m.affiche_message("Amy,bob") == "Hello, Amy and Bob"
-    assert m.affiche_message("Amy,Bob,Jerry") =="Hello, Amy, Bob and Jerry"
-    assert m.affiche_message("Amy,BOB,Jerry") == "Hello, Amy and Jerry. AND HELLO, BOB !"
-    assert m.affiche_message("bob     ,amy") == "Hello, Bob and Amy"
+def test_affiche_plusieurs_personnes_features_4_5_6_7_and_8():
+    assert m.afficher_message("Amy,bob") == "Hello, Amy and Bob"
+    assert m.afficher_message("Amy,Bob,Jerry") =="Hello, Amy, Bob and Jerry"
+    assert m.afficher_message("Amy,BOB,Jerry") == "Hello, Amy and Jerry. AND HELLO, BOB !"
+    assert m.afficher_message("bob     ,amy") == "Hello, Bob and Amy"
 
-def test_affiche_message_avec_particulariter():
-    assert m.affiche_message("!bob,amy") == "Hello, Amy"
-    assert m.affiche_message("bob,!bob,amy ,bob") == "Hello, Bob(X2) and Amy"
-    assert m.affiche_message("bob,!bob,amy ,bob, jean,alice,jules,albert") == "Hello, world !"
-    assert m.affiche_message("!bob, JULES,AMY,ALICE,CHARLOTTE,ANTOINE,MARTIN") == "HELLO, WORLD !"
-    assert m.affiche_message("bob,yoda,amy") == "Bob, Yoda and Amy, Hello"
-    assert m.affiche_message("bob,!bob,amy ,bob, jean,alice,jules,albert,yoda") == "World, Hello !"
+def test_affiche_message_avec_particularites_features_9_10_11_12_and_15():
+    assert m.afficher_message("!bob,amy") == "Hello, Amy"
+    assert m.afficher_message("bob,!bob,amy ,bob") == "Hello, Bob(X2) and Amy"
+    assert m.afficher_message("bob,!bob,amy ,bob, jean,alice,jules,albert") == "Hello, world !"
+    assert m.afficher_message("!bob, JULES,AMY,ALICE,CHARLOTTE,ANTOINE,MARTIN") == "HELLO, WORLD !"
+    assert m.afficher_message("bob,*alice, amy") == "Hello, Bob and Amy and our special guest Alice"
 
-def test_affiche_message_plusieur_particulariter():
-    assert m.affiche_message("!bob, JULES,AMY,ALICE,CHARLOTTE,ANTOINE,MARTIN,yoda") == "WORLD, HELLO !"
-    assert m.affiche_message("bob,*alice, amy") == "Hello, Bob and Amy and our special guest Alice"
+def test_affiche_message_avec_yoda_features_13_and_14():
+    assert m.afficher_message("bob,yoda,amy") == "Bob, Yoda and Amy, Hello"
+    assert m.afficher_message("bob,!bob,amy ,bob, jean,alice,jules,albert,yoda") == "World, Hello !"
+    assert m.afficher_message("!bob, JULES,AMY,ALICE,CHARLOTTE,ANTOINE,MARTIN,yoda") == "WORLD, HELLO !"
+
 
     
